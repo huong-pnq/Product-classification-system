b60ee90b261ab2961b3edc044bdc62c2b0577ad4
huong-pnq <152676206+huong-pnq@users.noreply.github.com>
2026-02-25 16:09:18 +0700

    Update Train.py

--- object-detection/venv/Train.py
+++ object-detection/venv/Train.py
@@ -6,10 +6,3 @@ model = YOLO("yolov8n.pt") # build a new model from scratch
 
 # #Use the model
 results = model.train(data="D:/object-detection/venv/config.yaml", epochs=500) # train the model
-
-
-# model_path = os.path.join('D:/object-detection/venv/runs/detect/train3/weights/best.pt')
-
-# # Load mô hình YOLO đã huấn luyện
-# model = YOLO(model_path) 
-# results = model.predict(source='D:/Downloads/anhnap1/anh7.jpg', save=True)
